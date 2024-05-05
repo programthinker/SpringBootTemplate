1ee06157d0d8d55fb91c1ce6c61df77a247a4947
programmer <zgycareer@gmail.com>
2024-05-05 21:23:28 +0800

    d

--- src/main/java/com/itheima/template/service/LogRepository.java
+++
@@ -1,7 +0,0 @@
-package com.itheima.template.service;
-
-import com.itheima.template.entity.Blog;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
-public interface LogRepository extends ElasticsearchRepository<Blog,String> {
-}
